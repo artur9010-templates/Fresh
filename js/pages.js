01a8facd2c5c99b7b79be95c43aef7a29625601a
Nyanide <ezx@dzikoysk.net>
2015-03-01 17:56:24 +0100

    Update pages.js

--- js/pages.js
+++ js/pages.js
@@ -7,10 +7,10 @@ function loadPage(page){
 	var m = i.height();
 	p.animate({opacity:0}, 500,function(){
 		p.html(i.html());
-		p.height(n);
+		p.height(m);
 	})
 	setTimeout(function(){
-		p.animate({height:m, opacity: 1}, 500)
+		p.animate({height:n, opacity: 1}, 500)
 	},501)
 //	p.animate({height:m}, 1000)
 	$('*[id^="pages-"]').each(function(){$(this).attr("class", "");})
