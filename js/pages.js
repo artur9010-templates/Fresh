162922c8741eb13fc9f7095139565ad7b39a6440
Nyanide <ezx@dzikoysk.net>
2015-03-01 17:48:57 +0100

    Update pages.js

--- js/pages.js
+++ js/pages.js
@@ -2,7 +2,7 @@ $( "#page" ).load( "pages/index.html" );
 
 function loadPage(page){
 	//$("#page").load( "pages/" + page + ".html" );
-	var i = $("</div>"), p = $("#page"), n = p.height();
+	var i = $("#k"), p = $("#page"), n = p.height();
 	i.load( "pages/" + page + ".html" );
 	var m = i.height();
 	p.load( "pages/" + page + ".html" );
