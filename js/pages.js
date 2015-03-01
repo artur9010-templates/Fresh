7213376ecb717e52c923da11e7f2718e4a57c92c
Nyanide <ezx@dzikoysk.net>
2015-03-01 16:48:41 +0100

    Update pages.js

--- js/pages.js
+++ js/pages.js
@@ -1,8 +1,8 @@
 var lastPage = "index";
-$( "#body" ).load( "pages/index.html" );
+$( "#page" ).load( "pages/index.html" );
 
 function loadPage(page){
-	$("#body").load( "pages/" + page + ".html" );
+	$("#page").load( "pages/" + page + ".html" );
 	$("#pages-" + lastPage).attr("class", "");
 	$("#pages-" + page).attr("class", "active");
 	lastPage = page;
