5aebe5efe4f9f4121c430340f3298c2ad78e1376
Nyanide <ezx@dzikoysk.net>
2015-03-01 16:52:27 +0100

    Update pages.js

--- js/pages.js
+++ js/pages.js
@@ -2,7 +2,6 @@ $( "#page" ).load( "pages/index.html" );
 
 function loadPage(page){
 	$("#page").load( "pages/" + page + ".html" );
-	for(var i = 0; i < $('*[id^="pages-"]').length;i++)
-		$(this).attr("class", "");
+	$('*[id^="pages-"]').each(function(){$(this).attr("class", "");})
 	$("#pages-" + page).attr("class", "active");
 }
