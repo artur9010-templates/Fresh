54f5089820461aba093808ade77b99a3e9fd18bc
BuildTools <unconfigured@null.spigotmc.org>
2016-03-06 14:04:36 +0100

    1.7 Update

--- js/serverstatus.js
+++ js/serverstatus.js
@@ -1,12 +1,12 @@
 MinecraftAPI.getServerStatus(serverIP, {
     port: serverPort // optional, only if you need a custom port
-  }, function (err, status) {
-    $("#serverIP").html(serverIP);
+}, function (err, status) {
+    //$("#serverIP").html(serverIP);
+    $("#serverIP").html("<input class=\"mc-input\" value='" + serverIP + "' disabled>")
     if (err) {
-      return document.querySelector('.server-status').innerHTML = "<span class=\"label label-danger\">Error :<</span>";
+        return document.querySelector('.server-status').innerHTML = "<span class=\"label label-danger\">Error :<</span>";
     }
 
     // you can change these to your own message!
-    //document.querySelector('.server-online').innerHTML = status.online ? 'Online' : 'Offline';
-    $("#serverStatus").html(status.online ? '<span class="label label-success">Server online</span>' : '<span class="label label-danger">Server offline</span>');
-  });
+    $("#serverStatus").html(status.online ? '<span class="label label-success">' + serverOnline + '</span>' : '<span class="label label-danger">' + serverOffline + '</span>');
+});
